8ae6ca52c0312a7144ef141347090e8ff0d6494e
Stonerider <bodia2006@ukr.net>
2017-01-27 11:39:26 +0200

    Initial commit

---
+++ application/controllers/Iqosrest.php
@@ -0,0 +1,23 @@
+﻿<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+require(APPPATH.'libraries/REST_Controller.php');
+use Restserver\Libraries\REST_Controller;
+
+class IqosRest extends REST_Controller {
+	
+	public function index_get()
+	{
+		$this->response('123456');
+	}
+
+	public function index_post()
+	{
+		$this->response('bugaga', 201);
+		
+	}
+}
---
+++ application/controllers/Newiqos.php
@@ -0,0 +1,28 @@
+﻿<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Newiqos extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "http://t-sales.com.ua/index.php/iqosrest");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+		curl_setopt($ch, CURLOPT_POST, TRUE);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		  "Content-Type: application/json"
+		));
+
+		$response = curl_exec($ch);
+		curl_close($ch);
+		var_dump($response);
+	}
+	
+}
